ca9678385cdede6999dbaa890ce219a610f76d7e
svenalpha <svalphaforsale@yahoo.com>
2024-02-22 17:53:06 +0700

    message

--- src/pages/Error404.jsx
+++
@@ -1,7 +0,0 @@
-export default function Error404() {
-    let msg = 'default message inside Error404'
-    try {
-      msg = process.env.MY_CUSTOM_SECRET || msg
-    } catch {}
-    return <h1>{msg}</h1>
-  }
